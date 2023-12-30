09386cd9ee55db4c651137e79706ff86b86935f3
Artur <artur.wlodarczyk12@gmail.com>
2023-12-30 17:00:56 +0100

    Aplikacja odczytuje poprawnie tekstowe dane z firestore i wyswietla je w odpowiednim miejscu

--- app/src/main/java/com/example/pshepishnickproject/RecipeAdapter.java
+++ app/src/main/java/com/example/pshepishnickproject/RecipeAdapter.java
@@ -1,4 +1,65 @@
 package com.example.pshepishnickproject;
 
-public class RecipeAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+    private List<Recipe> recipes;
+
+    public RecipeAdapter(List<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recipe, parent, false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+        holder.bind(recipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public static class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView titleTextView;
+        private TextView descriptionTextView;
+        private TextView difficultyTextView;
+        private ImageView photoImageView;
+
+        public RecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
+            difficultyTextView = itemView.findViewById(R.id.difficultyTextView);
+            photoImageView = itemView.findViewById(R.id.photoImageView);
+        }
+
+        public void bind(Recipe recipe) {
+            titleTextView.setText(recipe.getTitle());
+            descriptionTextView.setText(recipe.getDescription());
+            difficultyTextView.setText("Difficulty: " + recipe.getDifficulty());
+            // Load the photo using your preferred image loading library (e.g., Glide, Picasso)
+            // Example using Glide:
+            // Glide.with(itemView.getContext()).load(recipe.getPhotoUrl()).into(photoImageView);
+        }
+    }
 }
+
--- app/src/main/java/com/example/pshepishnickproject/recipesList.java
+++ app/src/main/java/com/example/pshepishnickproject/recipesList.java
@@ -1,35 +1,57 @@
 package com.example.pshepishnickproject;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class recipesList extends Fragment {
 
-    public recipesList() {
-        // Required empty public constructor
-    }
-
-    public static recipesList newInstance(String param1, String param2) {
-        recipesList fragment = new recipesList();
-
-        return fragment;
-    }
+    private RecyclerView recyclerView;
+    private RecipeAdapter recipeAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipes_list, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recipies_list, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Fetch recipes from Firestore and set up the adapter
+        fetchRecipesFromFirestore();
+
         return view;
+    }
+
+    private void fetchRecipesFromFirestore() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("recipes")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    List<Recipe> recipes = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        Recipe recipe = document.toObject(Recipe.class);
+                        recipes.add(recipe);
+                    }
+
+                    // Set up the adapter with the fetched recipes
+                    recipeAdapter = new RecipeAdapter(recipes);
+                    recyclerView.setAdapter(recipeAdapter);
+                })
+                .addOnFailureListener(e -> {
+                    // Handle the failure
+                    Log.e("RecipeListFragment", "Error fetching recipes", e);
+                });
     }
 }
